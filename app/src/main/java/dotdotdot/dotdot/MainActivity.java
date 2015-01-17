5162cf7e8cba05136a02e1b48f693014c238e66d
mtracy01 <matthetracy@gmail.com>
2015-01-17 10:58:21 -0500

    Minor removal of obsolete TODO

--- app/src/main/java/dotdotdot/dotdot/MainActivity.java
+++ app/src/main/java/dotdotdot/dotdot/MainActivity.java
@@ -58,8 +58,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void updateFiles() {
-        // TODO display all of the files in the files ArrayList
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,menuItems);
         list.setAdapter(adapter);
 
